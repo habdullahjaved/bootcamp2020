a7602c75c54012de859de3dd6890e3fb8b6c4a4e
habdullahjaved <58295162+habdullahjaved@users.noreply.github.com>
2020-12-28 20:59:27 +0500

    Update App.js Dinner.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world from react</h1>
+     <Dinner dishName="Chicken Karahi"  sweetDish="Kheer"/>
+      <hr/>
+     <Dinner dishName="Nihaari"  sweetDish="Jalebi"/>
+     <hr/>
+     <Dinner dishName="Chicken Biryani"  sweetDish="Gajar Ka Halwa"/>
     </div>
   );
 }
